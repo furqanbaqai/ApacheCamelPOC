9ce5967adf11e71f0e864bf8e05ff54ac733f781
Furqan Baqai <baqai.furqan@gmail.com>
2019-04-09 11:52:22 +0400

    Working route for reading and writing to ActiveMQ queue

--- JMS.RequestResponseApp/src/main/java/io/openfintechlab/mw/bootstrap/ActiveMQBootStrap.java
+++ JMS.RequestResponseApp/src/main/java/io/openfintechlab/mw/bootstrap/ActiveMQBootStrap.java
@@ -1,60 +1,42 @@
 package io.openfintechlab.mw.bootstrap;
 
-import io.openfintechlab.mw.routes.ActiveMQRoute;
+// import io.openfintechlab.mw.routes.ActiveMQRoute;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.camel.component.ActiveMQComponent;
+// import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 import javax.jms.ConnectionFactory;
 
 public class ActiveMQBootStrap {
 
     public static void main(String[] args) {
+        Main main = new Main();
+
         CamelContext context = new DefaultCamelContext();
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin", "admin", ActiveMQConnection.DEFAULT_BROKER_URL);
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin", "admin", ActiveMQConnection.DEFAULT_BROKER_URL); // Remote connection
+        // ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false"); // not working
         context.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
         try {
             context.addRoutes(new RouteBuilder() {
                 @Override
                 public void configure() throws Exception {
-                    from("activemq:JMS.SAMPLE.REQ")
+                    from("activemq:JMS.SAMPLE.REQ?asyncConsumer=true&concurrentConsumers=10")
                             .to("log:?level=INFO&showBody=true&showHeaders=true")
                             .to("activemq:JMS.SAMPLE.RLY")
                             ;
                 }
             });
             context.start();
-            Thread.sleep(5000);
+            Thread.sleep(50000);
             context.stop();
         } catch (Exception e) {
             // Do something here
             e.printStackTrace();
         }
-
-       /* CamelContext context = new DefaultCamelContext();
-        try {
-            context.addRoutes(new RouteBuilder() {
-                @Override
-                public void configure() throws Exception {
-                    from("file:data/input?noop=true")
-                            .to("file:data/output")
-                            ;
-                }
-            });
-
-            context.start();
-            Thread.sleep(5000);
-            context.stop();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error received: "+ e.getLocalizedMessage());
-        }*/
-
-
-
     }
 }
